04ee4fdc2c41a46c5828ac9323b1beddb03a4f6b
Chan Pyae Phyo Aung <114207295+chanpyaephyoaung@users.noreply.github.com>
2024-01-02 15:12:34 +0000

    Add auth users slice

---
+++ frontend/src/slices/authUserSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Use localstorage user account info if there is, else then null
+const userAccInfo = localStorage.getItem("userAccInfo")
+   ? JSON.parse(localStorage.getItem("userAccInfo"))
+   : null;
+
+const initialState = {
+   userAccInfo,
+};
+
+const authUserSlice = createSlice({
+   name: "authUser",
+   initialState,
+   reducers: {
+      setSignInDetails: (state, action) => {
+         const payloadDetails = action.payload;
+         state.userAccInfo = payloadDetails;
+
+         // Store in local storage
+         localStorage.setItem("userAccInfo", JSON.stringify(payloadDetails));
+      },
+   },
+});
+
+export const { setSignInDetails } = authUserSlice.actions;
+
+export default authUserSlice.reducer;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice.js";
 import shoppingCardSliceReducer from "./slices/shoppingCartSlice.js";
+import authUserSliceReducer from "./slices/authUserSlice.js";
 
 const store = configureStore({
    reducer: {
       [apiSlice.reducerPath]: apiSlice.reducer,
+      authUser: authUserSliceReducer,
       shoppingCart: shoppingCardSliceReducer,
    },
    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
